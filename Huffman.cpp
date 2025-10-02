006c051ba7bb86a29a20b943750511d5d178d9e6
ssugary <ssugary0@gmail.com>
2025-10-02 10:17:06 -0300

    Implementação do Descompactador

--- Huffman.cpp
+++ Huffman.cpp
@@ -9,7 +9,8 @@ std::string hf::PALAVRAS_CHAVE_CPP[NUM_PALAVRAS_CHAVE] = {      //array constant
 };
 
 std::vector<std::string> palavrasChaveEncontradas;          //vetor que guarda cada palavra chave que encontrar para evitar contar os caracteres dela.
-int Huffman::pertencenasubstring(std::vector<Huffman::substring> &substrings, size_t &i){
+
+int Huffman::pertenceNaSubstring(std::vector<Huffman::substring> &substrings, size_t &i){
     for(substring alvo :substrings){
         if(alvo.pos == i){
             return alvo.palavraint;
@@ -17,16 +18,38 @@ int Huffman::pertencenasubstring(std::vector<Huffman::substring> &substrings, si
     }
     return -1;
 }
-std::vector<Huffman::substring> Huffman::caçarsubstrings(std::string &alvo){
+
+void atualizar(std::string &alvo, size_t pos, size_t tamanho){
+    for(size_t i = pos; i < pos + tamanho; i++){
+        alvo[i] = 'x';
+    }
+}
+
+void Huffman::cacarNSubstrings(std::string &alvo, size_t i, std::vector<Huffman::substring> &substrings_encontradas){
+    if(alvo.find(nos[i]->caractere) !=std::string::npos){
+        size_t temp = alvo.find(nos[i]->caractere);
+        auto encontrada= new Huffman::substring(temp,i);
+        substrings_encontradas.push_back(*encontrada);
+        atualizar(alvo, temp, nos[i]->caractere.size());
+        cacarNSubstrings(alvo, i, substrings_encontradas);
+    }
+
+}
+
+
+std::vector<Huffman::substring> Huffman::cacarSubstrings(std::string &alvo){
     std::vector<Huffman::substring> substrings_encontradas;
+    std::string alvotemp = alvo;
     for(size_t i=NUM_CARACTERES_ASCII;i < NUM_CARACTERES_ASCII+NUM_PALAVRAS_CHAVE; i++){
-        if(alvo.find(nos[i]->caractere) !=std::string::npos){
-            auto encontrada= new Huffman::substring(alvo.find(nos[i]->caractere),i);
-            substrings_encontradas.push_back(*encontrada);
-        }
+        cacarNSubstrings(alvotemp, i, substrings_encontradas);
     }
     return substrings_encontradas;
 }
+
+
+
+
+
 void Huffman::encherArrayDeCaracteres(){
 
     for(size_t i{0}; i < NUM_CARACTERES_ASCII; i++){
@@ -42,21 +65,20 @@ void Huffman::encherArrayDePalavras() {                                 //apenas
     }
 }
 
-
-
-
-Huffman::Huffman(std::string entrada, std::string saida) { //construtor padrão
+Huffman::Huffman(std::string entrada, std::string entrada2) { //construtor padrão
 
     nomeDaEntrada = entrada;
-    nomeDaSaida = saida;
+    nomeDaEntrada2 = entrada2;
     encherArrayDeCaracteres();
     encherArrayDePalavras();
 }
+
 Huffman::Huffman(std::string entrada) { //construtor padrão
     nomeDaEntrada = entrada;
     encherArrayDeCaracteres();
     encherArrayDePalavras();
 }
+
 Huffman::~Huffman(){                                       //destrutor padrão
     for(size_t i{0}; i < NUM_CARACTERES_ASCII; i++){
         delete nos[i];
@@ -66,60 +88,49 @@ Huffman::~Huffman(){                                       //destrutor padrão
     }
 }
 
-void Huffman::lerPalavrasChave() {          //leitor de palavras chave
-    arquivoDeEntrada.open(nomeDaEntrada, std::fstream::in);
-    std::string palavra;
-
-    while (arquivoDeEntrada >> palavra) {           //lê palavra por palavra
-        for (size_t i = 0; i < NUM_PALAVRAS_CHAVE; ++i) {
-            if (PALAVRAS_CHAVE_CPP[i] == palavra) {     //caso for uma palavra chave, aumenta a frequência dela e adiciona no vetor delas.
-                nos[NUM_CARACTERES_ASCII + i]->freq++;
-                palavrasChaveEncontradas.push_back(palavra);
-                break;
-            }
-        }
-    }
-
-    arquivoDeEntrada.close();
-}
 
 
 void Huffman::montarTabela() {
     std::ifstream arquivo(nomeDaEntrada);
     std::string linha;
+    size_t poslinha=0;
     while(std::getline(arquivo,linha)){
-         auto substrings=caçarsubstrings(linha);
+
+        nos[10]->freq++;
+
+         auto substrings=cacarSubstrings(linha);
         for(size_t i=0;i<linha.size();i++){
-            int teste=pertencenasubstring(substrings,i);
+            int teste=pertenceNaSubstring(substrings,i);
             if(teste >= 0){
                 nos[teste]->freq++;
                 i+=nos[teste]->caractere.size()-1;
-            }
+            }       
             else{
                 unsigned char ch = linha[i];
-                if (nos[ch]) {
+                if (nos[ch] && ch < NUM_CARACTERES_ASCII) {
                     nos[ch]->freq++;
                 } else {
-                    //std::cerr << "Caracter inválido ou não encontrado";
+                    std::cerr << poslinha << " " << i << std::endl << linha[i] << " " <<"Caracter inválido ou não encontrado" << std::endl; 
                 }
             }
         }
+        poslinha++;
     }
     arquivo.close();
 }
 void Huffman::criarArquivoTabela(){
     std::ofstream arquivo("tabela.txt");
     for(size_t i=0;i<NUM_CARACTERES_ASCII+NUM_PALAVRAS_CHAVE;i++){
-        if(nos[i]->freq>0){
-            arquivo<<i<<" "<<nos[i]->freq<<" ";
+        if(nos[i]->freq > 0){
+            arquivo << i << " "<<nos[i]->freq << " ";
         }
     }
     arquivo.close();
 }
 void Huffman::lerArquivoTabela(){
-    arquivoDeEntrada.open(nomeDaSaida, std::fstream::in);
+    arquivoDeEntrada.open(nomeDaEntrada2, std::fstream::in);
     std::string palavra;
-    size_t i=0;
+    size_t i = 0;
     bool index =true;
     while (arquivoDeEntrada >> palavra) { 
         if(index){
@@ -137,17 +148,9 @@ void Huffman::lerArquivoTabela(){
         }
     }
 }
-void Huffman::printTabela(){            //apenas para teste: printa todos os nós com suas respectivas frequencias.
-    while(!contador.priorityQueue.empty()){
-        no::No* no = contador.priorityQueue.top();
-        contador.priorityQueue.pop();
-        std::cout << "( "<< no->caractere << ", " << no->freq << ", "<< no->codigo<< ") ";
-        std::cout << std::endl;
-    }
-}
 no::No* Huffman::montarArvore(){ //com base na frequência monta a arvore
     cf::ContadorDeFrequencia copia; //cria uma cópia a fim de preserva a lista original
-    copia.priorityQueue=contador.priorityQueue;
+    copia.priorityQueue = contador.priorityQueue;
     while(copia.priorityQueue.size()>1){//pegamos de dois em dois elementos até forma o nó raiz por isso a condição de parada é quando chegar a 1
         no::No* no1=new no::No(copia.priorityQueue.top()); // cria um ponteiro que copia o primeiro elemento
         copia.priorityQueue.pop();
@@ -167,22 +170,37 @@ int Huffman::mostrarArvore(no::No* nodo){//mostra a arvore mas toda feia =(
         return 0;
     }
 }
-std::string Huffman::fazercodigo(std::string alvo,no::No* busca,std::string codigo){//cria o código de 1 elemento apenas
+void Huffman::printTabela(){            //apenas para teste: printa todos os nós com suas respectivas frequencias.
+    while(!contador.priorityQueue.empty()){
+        no::No* no = contador.priorityQueue.top();
+        contador.priorityQueue.pop();
+        if(no->caractere == "\n"){
+        std::cout << "( "<< "\\n "<< ", " << no->freq << ", "<< no->codigo<< ") ";
+        std::cout << std::endl;
+        continue;
+        }
+        std::cout << "( "<< no->caractere << ", " << no->freq << ", "<< no->codigo<< ") ";
+        std::cout << std::endl;
+    }
+}
+
+std::string Huffman::fazerCodigo(std::string alvo,no::No* busca,std::string codigo){//cria o código de 1 elemento apenas
+    if(busca == nullptr)return "";
     if(alvo==busca->caractere){//achei o elemento então retorno seu código
         return codigo;
     }
-    else if(busca->filhoDir == nullptr){//veja a chamada e recursiva e volte. Voltou? chamamos a função em todos os nóss, se caso chegarmos em uma folha e ela não é o alvo apenas retornamos uma string vazia para não influenciar no resultado final
+    else if(busca->filhoEsq == nullptr){//veja a chamada e recursiva e volte. Voltou? chamamos a função em todos os nóss, se caso chegarmos em uma folha e ela não é o alvo apenas retornamos uma string vazia para não influenciar no resultado final
         return "";
     }
     else{//por fim, chegamos aqui e nada.Agora, vamos para nó a esquerda e para o nó a direita, também veja que eles já carregam uma informação sobre o caminho de onde vieram
-        return fazercodigo(alvo,busca->filhoEsq, codigo+"0") + fazercodigo(alvo,busca->filhoDir,codigo+"1");
+        return fazerCodigo(alvo,busca->filhoEsq, codigo+"0") + fazerCodigo(alvo,busca->filhoDir,codigo+"1");
     }
 }
 void Huffman::codificar(){//cria o código de todos
     cf::ContadorDeFrequencia copia;// copiamos para não alterar a original, ela pode ser útil mas tarde
     copia.priorityQueue=contador.priorityQueue;
     while(!copia.priorityQueue.empty()){//basicamente aplica a função anterior em todos os elementos
-        copia.priorityQueue.top()->codigo=fazercodigo(copia.priorityQueue.top()->caractere,ArvoreDeHuffman,"");
+        copia.priorityQueue.top()->codigo=fazerCodigo(copia.priorityQueue.top()->caractere,ArvoreDeHuffman,"");
         copia.priorityQueue.pop();
     }
 }
@@ -190,10 +208,16 @@ void Huffman::compactar(){
     std::ifstream arquivol(nomeDaEntrada);
     std::ofstream arquivow("algo.bin");
     std::string linha;
+
+    std::string codigo_quebra_linha = "";
+    if (nos[10] && !nos[10]->codigo.empty()){
+        codigo_quebra_linha = nos[10]->codigo;
+    }
+
     while (std::getline(arquivol, linha)){
-        auto substrings=caçarsubstrings(linha);
+        auto substrings=cacarSubstrings(linha);
         for(size_t i=0;i<linha.size();i++){
-            int teste=pertencenasubstring(substrings,i);
+            int teste=pertenceNaSubstring(substrings,i);
             if(teste >= 0){
                 arquivow << nos[teste]->codigo;
                 i+=nos[teste]->caractere.size()-1;
@@ -207,8 +231,43 @@ void Huffman::compactar(){
                 }
             }
         }
-        arquivow<<"\n";
+        if(!codigo_quebra_linha.empty()){
+            arquivow << codigo_quebra_linha;
+        }
+        
     }
     arquivow.close();
     arquivol.close();
 }
+
+
+void Huffman::descompactar(){
+    std::ifstream ifs(nomeDaEntrada);
+    std::ofstream ofs("original.txt");
+    std::string linha;
+    
+    std::getline(ifs, linha);
+
+    no::No *aux = ArvoreDeHuffman;
+
+    for(char c : linha){
+        if(aux->filhoEsq == nullptr){
+            ofs << aux->caractere;
+            aux = ArvoreDeHuffman;
+        }
+
+        if(c == '0'){
+            aux = aux->filhoEsq;
+        }
+        if(c == '1'){
+            aux = aux->filhoDir;
+        }
+    }
+
+    ifs.close();
+    ofs.close();
+
+
+}
+
+
--- Huffman.hpp
+++ Huffman.hpp
@@ -27,31 +27,33 @@ namespace hf {
             this->palavraint=palavraint;
         }
         };
-        int pertencenasubstring(std::vector<substring> &,size_t &);
-        std::vector<substring> caçarsubstrings(std::string&);
+        int pertenceNaSubstring(std::vector<substring> &,size_t &);
+        std::vector<substring> cacarSubstrings(std::string&);
         no::No* nos[NUM_CARACTERES_ASCII + NUM_PALAVRAS_CHAVE]; //array de todos os nós e possiveis caracteres + possiveis palavras chave
         cf::ContadorDeFrequencia contador;  //priority_queue para organizar os nós.
         no::No* ArvoreDeHuffman;
         std::fstream arquivoDeEntrada, arquivoDeSaida; //arquivo de saida está inutilizavel ainda, mas o de entrada é o que a gente vai ler
         std::string nomeDaEntrada; //nome do arquivo de entrada
-        std::string nomeDaSaida; //nome do arquivo de saída
+        std::string nomeDaEntrada2; //nome do arquivo de saída
         void encherArrayDeCaracteres(); // "construtores" do array de nós
         void encherArrayDePalavras();
+
         public:
     
         Huffman(std::string, std::string); //construtor da classe
         Huffman(std::string); //construtor da classe
         ~Huffman();                         // destrutor da classe
-        void lerPalavrasChave();            // função que pega as palavras chave do arquivo de leitura
         void montarTabela();                //função que pega os caracteres + palavras chave do arquivo de leitura e joga na priority_queue
         void criarArquivoTabela();
         void lerArquivoTabela();
         void printTabela();                 //apenas para testes: printa todos os nós em ordem de prioridade
         no::No* montarArvore();
         int mostrarArvore(no::No* nodo);
-        std::string fazercodigo(std::string alvo, no::No* busca,std::string codigo);
+        std::string fazerCodigo(std::string alvo, no::No* busca,std::string codigo);
         void codificar();
         void compactar();
+        void cacarNSubstrings(std::string &substring, size_t n, std::vector<Huffman::substring> &substrings_encontradas);
+        void descompactar();
     };
 
 
